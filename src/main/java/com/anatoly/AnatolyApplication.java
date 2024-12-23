10238a59ea917c14135f0786a89c011071406b06
Tolynet <anatoly.slowetzky@gmail.com>
2024-12-23 11:17:16 -0300

    Adding Springboot initializer files

---
+++ src/main/java/com/anatoly/AnatolyApplication.java
@@ -0,0 +1,13 @@
+package com.anatoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnatolyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnatolyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anatoly/AnatolyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anatoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnatolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
